da9bafd7a701802f34549b897fbbccccbf9fbefb
ky-official <65351617+ky-official@users.noreply.github.com>
2020-10-15 15:38:43 +0100

    ---

---
+++ src/main/kotlin/com/example/test/TestApplication.kt
@@ -0,0 +1,20 @@
+package com.example.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+@RestController
+class TestApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestApplication>(*args)
+}
+@GetMapping("/")
+@CrossOrigin
+fun testController(): String {
+	return "somethhing"
+}
