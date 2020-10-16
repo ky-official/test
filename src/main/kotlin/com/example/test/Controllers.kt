b22a07d4989749c28107e8e924f6650db5090bde
ky-official <65351617+ky-official@users.noreply.github.com>
2020-10-16 14:36:26 +0100

    ---

--- src/main/kotlin/com/example/test/Controllers.kt
+++ src/main/kotlin/com/example/test/Controllers.kt
@@ -3,10 +3,11 @@ package com.example.test
 import com.google.cloud.storage.BlobId
 import com.google.cloud.storage.Storage
 import com.google.cloud.storage.StorageOptions
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
+
 
 @RestController
 class Controllers {
@@ -18,9 +19,16 @@ class Controllers {
     @CrossOrigin
     fun testController(): String {
 
-
         storage = StorageOptions.newBuilder().setProjectId("notional-portal-292519").build().service
         var blob = storage.get(BlobId.of("testing_bucket_data","doc.txt"))
-        return blob.contentType
+
+        var temp = File("/tmp")
+        blob.downloadTo(temp.toPath())
+
+        var txt = File("/tmp/doc.txt")
+
+
+
+        return txt.toString()
     }
 }
